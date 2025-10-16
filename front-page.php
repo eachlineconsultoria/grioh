2eedd64741844fb75e63495c99c268374a7ff8c4
Wagner <wagnerbeethoven@gmail.com>
2025-10-15 21:02:04 -0300

    feat: Adicionar seções de parceiros e artigos na página inicial, refatorar loop de cases e otimizar estilos de cards

--- front-page.php
+++ front-page.php
@@ -56,8 +56,49 @@ $icon_choice = get_field('custom_icon_choice');
   </section>
 
   <?php require_once get_template_directory() . '/section/partners.php'; ?>
-  <?php require_once get_template_directory() . '/loop/cases.php'; ?>
+
+  <section id="partners" class="container section-container cases">
+    <header class="section-header d-flex align-items-center justify-content-center justify-content-md-between">
+      <h2 class="section-title">Clientes</h2>
+
+      <a href="
+    <?php
+    $slug = 'historias-sucesso'; // substitua pelo slug desejado
+    
+    $category = get_category_by_slug($slug);
+    if ($category) {
+      echo get_category_link($category->term_id);
+    }
+    ?>" class="card-link link link-primario">Acesse todos os cases<i class="ms-2 fa-solid fa-arrow-right"></i></a>
+
+    </header>
+    <?php require_once get_template_directory() . '/loop/cases.php'; ?>
+  </section>
+
   <?php require_once get_template_directory() . '/section/cta.php'; ?>
+
+
+
+  <section id="articles" class="container section-container articles">
+    <header class="section-header d-flex align-items-center justify-content-center justify-content-md-between">
+      <h2 class="section-title">Artigos</h2>
+
+      <a href="
+    <?php
+    $slug = 'artigos';
+    
+    $category = get_category_by_slug($slug);
+    if ($category) {
+      echo get_category_link($category->term_id);
+    }
+    ?>" class="card-link link link-primario">Leia todos os artigos<i class="ms-2 fa-solid fa-arrow-right"></i></a>
+
+    </header>
+    <?php require_once get_template_directory() . '/loop/articles.php'; ?>
+  </section>
+
+
+
 </main>
 
 <?php
--- loop/articles.php
+++ loop/articles.php
@@ -2,20 +2,20 @@
 $args = array(
   'post_type' => 'post',
   'posts_per_page' => 3,
-  'category_name' => 'historias-sucesso',
+  'category_name' => 'artigos',
   'orderby' => 'date',
   'order' => 'DESC',
 );
 
-$cases_query = new WP_Query($args);
+$articles_query = new WP_Query($args);
 ?>
 
-<?php if ($cases_query->have_posts()): ?>
+<?php if ($articles_query->have_posts()): ?>
   <div class="row">
-    <?php while ($cases_query->have_posts()):
-      $cases_query->the_post(); ?>
+    <?php while ($articles_query->have_posts()):
+      $articles_query->the_post(); ?>
       <div class="col-12 col-md-4">
-        <article class="card  border rounded h-100">
+        <article class="card border rounded h-100">
           <figure class="m-0 card-container">
             <?php if (has_post_thumbnail()): ?>
 
@@ -23,20 +23,25 @@ $cases_query = new WP_Query($args);
                 <?php the_post_thumbnail('medium_large', ['class' => 'img-fluid card-image object-fit-cover rounded-top w-100']); ?>
               </a>
             <?php else: ?>
-              <img src="<?php echo get_template_directory_uri(); ?>/assets/img/case-default.jpg"
+              <img src="<?php echo get_template_directory_uri(); ?>/assets/img/card-default.jpg"
                 class="img-fluid card-image rounded-top w-100" alt="Imagem padrão">
             <?php endif; ?>
           </figure>
-          <div class="card-body case-body p-3">
+          <div class="card-body card-body p-3">
             <h3 class="card-title mb-0">
-              <a href="<?php the_permalink(); ?>" class="card-link text-decoration-none">
+              <a href="<?php the_permalink(); ?>" class="card-link">
               <?php the_title(); ?>
             </a></h3>
+            <p class="card-meta mb-0">
+              Por <strong><?php the_author(); ?></strong> &bull; <?php the_time('d/m/Y'); ?>
+            </p>
             <p class="card-excerpt mb-0"><?php echo get_the_excerpt(); ?></p>
+
+
           </div>
           <footer class="border-0 card-footer card-footer">
-            <a href="<?php the_permalink(); ?>" class="card-link link link-primario">
-              Leia mais<i class="ms-2 fa-solid fa-arrow-right"></i>
+            <a href="<?php the_permalink(); ?>" class=" link link-primario">
+              Leia o texto<i class="ms-2 fa-solid fa-arrow-right"></i>
             </a>
           </footer>
         </article>
